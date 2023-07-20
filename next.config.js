79c1046bdc1de3968a5f43705ad0e280a1a40073
alexdalat <alexdalat@gmail.com>
2023-07-20 18:20:43 -0400

    prepare for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePaht: "/ccc",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
